8337f2a6713f4053433cfb71af2160732847cc10
AndreMcqu <104978076+AndreMcqu@users.noreply.github.com>
2023-06-12 12:12:05 +0200

    Wip

--- app/screens/Calendar.tsx
+++ app/screens/Calendar.tsx
@@ -1,36 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { CalendarList, Agenda } from 'react-native-calendars';
 import moment from 'moment';
 
-interface Appointment {
-  id: number;
-  streetName: string;
-  date: string;
+// interface Appointment {
+//   id: number;
+//   streetName: string;
+//   date: string;
+// }
+
+export type Client = {
+    first_name?: string
+    last_name?: string
+}
+
+export type AppointmentType = {
+    id?: number
+    idMission: number
+    date: string
+    startHour: string
+    endHour: string
+    streetName: string
+    streetNumber?: string
+    postCode: string
+    city: string,
+    mission: {
+        client:Client
+        idClient?: number
+        idReccurence?: number
+    }
 }
 
 const Calendar: React.FC = () => {
-  const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const [appointments, setAppointments] = useState<AppointmentType[]>([]);
   const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'));
 
   useEffect(() => {
     fetchAppointments();
   }, []);
 
+
+
   const fetchAppointments = async () => {
     try {
-      const response = await fetch('https://ae8f-31-32-43-205.ngrok-free.app/customers/1/appointments');
+      const response = await fetch('https://ae8f-31-32-43-205.ngrok-free.app/carers/3/appointments');
       const data = await response.json();
-      setAppointments(data.appointments);
+      setAppointments(data);
+      console.log(data);
     } catch (error) {
       console.error('Error fetching appointments:', error);
     }
   };
 
-  const renderAppointmentItem = ({ item }: { item: Appointment }) => {
+  console.log('Fetching appointments',appointments);
+  
+
+  const renderAppointmentItem = ({ item }: { item: AppointmentType }) => {
     return (
       <TouchableOpacity>
-        <Text>{item.streetName}</Text>
+        <Text>{item.id}</Text>
       </TouchableOpacity>
     );
   };
