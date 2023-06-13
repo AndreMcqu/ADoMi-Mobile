8b78c5ba63f16d4e15525100b432ebddaed5b987
TheoSoko <theo.sokolowsky@gmail.com>
2023-06-13 12:12:53 +0200

    Calendar appointments - main functionalities

--- app/screens/Calendar.tsx
+++ app/screens/Calendar.tsx
@@ -1,48 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Pressable } from 'react-native';
 import { Calendar, CalendarList, Agenda, AgendaEntry, AgendaSchedule } from 'react-native-calendars'
+import { FontAwesome } from "@expo/vector-icons";
 import type { StackScreenProps } from '@react-navigation/stack';
 import type { CalendarStackParamList, } from '../router/StackNavCalendar';
 import type { AppointmentType } from '../types/componentTypes'
+import {centerVertical, centerHorizontal} from '../dimensions/dimensions'
+import AppointmentModal from '../components/appointmentComponents/appointmentModal'
 import moment from 'moment';
 
 type props = StackScreenProps<CalendarStackParamList, 'Calendar'>
 
-interface Appointment {
-  id: number;
-  streetName: string;
-  date: string;
-}
-
-
-/*
-const entries1: AgendaEntry[] = [
-  {
-    name: 'John Doe',
-    height: 50,
-    day: 'Monday',
-  },
-  {
-    name: 'Yvette Dumont',
-    height: 15,
-    day: 'Monday',
-  }
-]
-const entries2: AgendaEntry[] = [
-  {
-    name: 'John Doe',
-    height: 100,
-    day: 'Friday',
-  }
-]
-
-const schedule: AgendaSchedule = {
-  '2023-05-01': entries1,
-  '2023-05-05': entries2
-}
-*/
-
-const appointmentExample = [
+const appointmentsExample = [
   {
     city: "Versailles", date: "2023-06-14T11:00:00.000Z", endHour: '15:00:00', id: 1, idMission: 1, mission: { idClient: 1, idRecurence: 1 }
     , postCode: "78000", startHour: "14:00:00", streetName: "Rue Jean Jaures", streetNumber: "2"
@@ -52,30 +21,47 @@ const appointmentExample = [
     , postCode: "78000", startHour: "16:00:00", streetName: "rue Albert Camus", streetNumber: "25"
   },
   {
-    city: "Versailles", date: "2023-06-15T11:00:00.000Z", endHour: "17:00:00", id: 2, idMission: 2, mission: { idClient: 1, idRecurence: 2}
-    , postCode: "78000", startHour: "16:00:00", streetName: "rue Albert Camus", streetNumber: "25"
+    city: "Buc", date: "2023-06-15T11:00:00.000Z", endHour: "10:00:00", id: 2, idMission: 2, mission: { idClient: 1, idRecurence: 2}
+    , postCode: "78117", startHour: "08:45:00", streetName: "Avenue du troubadour", streetNumber: "73"
+  },
+  {
+    city: "Buc", date: "2023-06-15T11:00:00.000Z", endHour: "20:00:00", id: 2, idMission: 2, mission: { idClient: 1, idRecurence: 2}
+    , postCode: "78117", startHour: "19:30:00", streetName: "Avenue du machin", streetNumber: "73"
   }
+
 ]
 
-const getDates = (apptList: Partial<AppointmentType>[]) => {
-  let apptDates: {[key: string]: {selected: boolean, selectedColor: string}} = {}
-  apptList.forEach(appt => {
-    apptDates[moment(appt.date).format('YYYY-MM-DD')] = {selected: true, selectedColor: 'pink'}
-  })
-  return apptDates
+const appts = appointmentsExample.map((appt) => { return { ...appt , date: moment(appt.date).format('YYYY-MM-DD') } })
+
+const getMarkedDates = (apptList: AppointmentType[]) => {
+    let apptDates: {[key: string]: {selected: boolean, selectedColor: string}} = {}
+    apptList.forEach(appt => {
+      apptDates[appt.date] = {selected: true, selectedColor: 'pink'}
+    })
+    return apptDates
 }
-const allMarkedDates = getDates(appointmentExample)
-//console.warn(allMarkedDates)
+const allMarkedDates = getMarkedDates(appts)
+
+const getApptFromDate = (date: string, appts: AppointmentType[]): AppointmentType[] => {
+    const fetchedAppts: AppointmentType[] = []
+    appts.forEach((appt) => {
+        if (appt.date == date) fetchedAppts.push(appt)
+    })
+    return fetchedAppts
+}
 
 export default function MyCalendar ({route, navigation}: props ) {
-  const [appointments, setAppointments] = useState<Appointment[]>([]);
-  const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'));
-    
-  const onDayPress = (day: any) => {
-    console.log(day);
-    setSelectedDate(day.dateString);
-  }
+  const [appointments, setAppointments] = useState<AppointmentType[]>([])
+  const [modalDisplay, setModalDisplay] = useState(false)
+  const [apptCount, setApptCount] = useState(0)
 
+
+  const onDayPress = (day: any) => {
+    if (allMarkedDates[day.dateString]){
+      setAppointments(getApptFromDate(day.dateString, appts))
+      setModalDisplay(true)
+    }
+  }
 
 /*
   useEffect(() => {
@@ -92,10 +78,10 @@ export default function MyCalendar ({route, navigation}: props ) {
     }
   };
 */
+
   const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
     return <Text style={{fontSize: 50}}> NOM DU CLIENT </Text>
   }
-
 
 
   /* VIEW */
@@ -109,15 +95,37 @@ export default function MyCalendar ({route, navigation}: props ) {
           onDayPress={onDayPress}
         />
         <View style={{
-          backgroundColor: 'lightgrey', 
+          backgroundColor: '#E8E8E8', 
           position: 'absolute',
-          top 
+          height: 290,
+          width: 290,
+          top: centerVertical(290) - 20,
+          left: centerHorizontal(290),
           borderRadius: 10,
-          zIndex:10, 
-          padding: 120,
+          zIndex: 2, 
+          display: modalDisplay ? 'flex' : 'none'
           }}>
+              <Text style={{fontSize: 21, fontWeight: '700', alignSelf: 'center', paddingTop: 21, marginBottom: -26}}>Rdv n° {apptCount+1}</Text>
+              <TouchableOpacity onPress={() => { setModalDisplay(false); setApptCount(0) }} style={s.closeButton}>
+                  <FontAwesome name="close" color="black" size={28}/>
+              </TouchableOpacity>
+              <View style={{display: 'flex', flexDirection: 'row', justifyContent:'space-evenly'}}>
+                  <TouchableOpacity onPress={() => apptCount > 0 && setApptCount(apptCount - 1)} style={s.leftArrowModal}>
+                      <FontAwesome name="chevron-left" color="black" size={24}/>
+                  </TouchableOpacity>
+                  <View style={{backgroundColor: 'white', padding: 25, borderRadius: 7}}>
+                      <Text style={s.modalText}>{appointments[apptCount]?.date}</Text>
+                      <Text style={s.modalText}>{appointments[apptCount]?.city}</Text>
+                      <Text style={s.modalText}>{appointments[apptCount]?.streetName}</Text>
+                      <Text style={s.modalText}> {appointments[apptCount] ? "De " + appointments[apptCount]?.startHour + " à " + appointments[apptCount]?.endHour : null}</Text>
+                  </View>
+                  <TouchableOpacity onPress={() => apptCount < appointments.length-1 && setApptCount(apptCount + 1)} style={s.rightArrowModal}>
+                      <FontAwesome name="chevron-right" color="black" size={24}/>
+                  </TouchableOpacity>
+              </View>
 
           </View>
+
         {
         /*
           <Agenda
@@ -170,5 +178,23 @@ const s = StyleSheet.create({
     fontSize: 15,
     fontWeight: '500',
     alignSelf: 'center',
+  },
+  closeButton:{
+    alignSelf:"flex-end",
+    paddingHorizontal: 17,
+    paddingBottom: 25,
+  },
+  leftArrowModal:{
+    alignSelf: "center",
+    paddingHorizontal: 15,
+  },
+  rightArrowModal:{
+    alignSelf: "center",
+    paddingHorizontal: 15,
+  },
+  modalText: {
+    fontSize: 16,
+
   }
+
 })
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -12,7 +12,6 @@ export default function Home (){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'pink',
         alignItems: 'center',
         paddingTop: 70,
 
